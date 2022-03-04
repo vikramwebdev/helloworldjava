8577f5ff2b80878ec6be91a1abb514dfcf8ddbb0
novarun <36502756+novarun@users.noreply.github.com>
2022-03-05 00:09:39 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello Worldfrom Github cron 2.);
+    System.out.println("Hello Worldfrom Github cron 2.");
     }
 }
