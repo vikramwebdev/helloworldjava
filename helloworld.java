a22049135ba5b2d28aab9b183072519019454b95
Novarun Deb <36502756+novarun@users.noreply.github.com>
2022-10-19 09:37:37 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello Worldfrom Github by Webhook.");
+    System.out.println("Hello World Sample Code.");
     }
 }
