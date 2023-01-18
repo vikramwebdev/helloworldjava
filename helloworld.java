54fce73db66039232930a9d3685d0d80b37f8d65
Oracle0Zero <112552488+Oracle0Zero@users.noreply.github.com>
2023-01-18 16:35:24 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World Sample Code. After change");
+    System.out.println("Hello World Sample Code.");
     }
 }
